c26334f1603abcb50328186afc1ab8d821c2335a
syed-imran2 <syed.imran@neoito.com>
2025-03-10 15:31:02 +0530

    completed the comparisons tasks

---
+++ 2.9 comparisons/comparisons.js
@@ -0,0 +1,7 @@
+alert(5 > 4); // True
+alert("apple" > "pineapple"); // False
+alert("2" > "12"); // False
+alert(undefined == null); // True
+alert(undefined === null); // False
+alert(null == "\n0\n"); // False
+alert(null === +"\n0\n"); // False
